0a38a92c008cfe0931dfbe633ab6ef00bf205649
Radzi <imradzi@gmail.com>
2026-02-06 12:24:49 +0800

    remove ShowLog in favor of LOG_INFO; remove precompiled headers

--- logger.cpp
+++ logger.cpp
@@ -3,10 +3,10 @@
 
 TimeTracker::TimeTracker(const std::string &m) : msg(m) {
   tStart = std::chrono::steady_clock::now();
-  ShowLog(fmt::format("{} started.", msg));
+  LOG_INFO("{} started.", msg));
 }
 
 TimeTracker::~TimeTracker() {
   auto ts = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - tStart);
-  ShowLog(fmt::format("{} completed. Elapsed {} ms.", msg, ts.count()));
+  LOG_INFO("{} completed. Elapsed {} ms.", msg, ts.count()));
 }
--- logger.h
+++ logger.h
@@ -6,9 +6,9 @@ public:
 };
 
 inline void wpLogMessage(const std::string &s) { LOG_INFO(s); }
-inline bool ShowLog(const std::string &t) {LOG_INFO(t); return true; }
+inline bool LOG_INFO(const std::string &t) {LOG_INFO(t); return true; }
 
-inline bool ShowLogOnVerboseDetail(const std::string &str) {ShowLog(str); return true; }
+inline bool LOG_INFO(const std::string &str) {LOG_INFO(str); return true; }
 
 class TimeTracker {
   std::chrono::steady_clock::time_point tStart;
