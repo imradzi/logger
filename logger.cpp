36dcba2325bff83ae7d80508158b6d53d9307224
Radzi <imradzi@gmail.com>
2026-03-23 23:45:21 +0800

    fixed moving logger out of wpsql

--- logger.cpp
+++ logger.cpp
@@ -3,10 +3,10 @@
 
 TimeTracker::TimeTracker(const std::string &m) : msg(m) {
   tStart = std::chrono::steady_clock::now();
-  LOG_INFO("{} started.", msg));
+  LOG_INFO("{} started.", msg);
 }
 
 TimeTracker::~TimeTracker() {
   auto ts = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - tStart);
-  LOG_INFO("{} completed. Elapsed {} ms.", msg, ts.count()));
+  LOG_INFO("{} completed. Elapsed {} ms.", msg, ts.count());
 }
--- logger.h
+++ logger.h
@@ -8,8 +8,6 @@ public:
 inline void wpLogMessage(const std::string &s) { LOG_INFO(s); }
 inline bool LOG_INFO(const std::string &t) {LOG_INFO(t); return true; }
 
-inline bool LOG_INFO(const std::string &str) {LOG_INFO(str); return true; }
-
 class TimeTracker {
   std::chrono::steady_clock::time_point tStart;
   std::string msg;
